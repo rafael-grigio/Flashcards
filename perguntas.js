27b94f9067c7756350f8c92926837ac6adea72c4
rafael-grigio <166824750+rafael-grigio@users.noreply.github.com>
2024-11-08 08:27:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é mitose?',
+    'Divisão celular que gera celulas idênticas.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é clorofila?',
+    'Pigmento responsável pela fotossíntese.'
+)
+
+criaCartao(
+    'História',
+    'O que foi o Renascimento?',
+    'Movimento cultural que valorizava a arte e ciência.(séculos XIV-XVI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
